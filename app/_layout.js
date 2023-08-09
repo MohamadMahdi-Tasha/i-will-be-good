a6b1d78cc87793d475ed64112fc2f8672fcabe4c
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-09 19:47:54 +0330

    added layout of pages

---
+++ app/_layout.js
@@ -0,0 +1,20 @@
+// Codes By Mahdi Tasha
+// Importing Part
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
+
+// Creating And Exporting Layout Of Pages
+export default function Layout() {
+    // Loading Fonts
+    const [fontsAreLoaded] = useFonts({
+        RobotRegular: require('../assets/fonts/roboto/Roboto-Regular.ttf'),
+        RobotBold: require('../assets/fonts/roboto/Roboto-Bold.ttf'),
+    })
+
+    // Returning JSX
+    return (
+        <Stack initialRouteName="Home">
+            <Stack.Screen name={'Home'} />
+        </Stack>
+    );
+}
